714e79099e57bd0aa967e786f7a04a5c6119dd31
kodeline <kodeline@hotmail.com>
2019-08-19 13:04:52 -0300

    Version 1.0

---
+++ img/trivia/thimble-project/_/js/mi_codigo.js
@@ -0,0 +1,21 @@
+/* 	
+Archivo mi_codigo.js
+En este archivo programaremos el código correspondiente
+al juego de Trivia.
+ */
+
+let	indice_pregunta_actual;
+let	total_puntos;
+
+indice_pregunta_actual = 0;
+total_puntos = 10;
+
+const nombre_alumno = "Nombre Alumno";
+const maximo_preguntas_por_jugada = 3;
+const puntos_resultado_bien = 2;
+
+console.log(nombre_alumno);
+console.log(maximo_preguntas_por_jugada);
+console.log(puntos_resultado_bien);
+console.log(indice_pregunta_actual);
+console.log(total_puntos);
